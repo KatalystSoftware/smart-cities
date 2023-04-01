895f68efe5419a3382ebaf993040ff553bfac020
Mikael Siidorow <mikael.siidorow@aalto.fi>
2023-04-01 18:03:44 +0300

    Properly apply disabled styling for buttons

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -8,14 +8,14 @@ const Home: NextPage = () => {
     <div className="flex flex-col gap-8 text-xl text-green-950">
       <Link
         href="home/gallery"
-        className="bg-gradient-rd from-green-400 via-green-400 to-green-500 flex gap-4 rounded-lg bg-green-300 px-4 py-6 shadow-md drop-shadow-lg disabled:bg-stone-400 disabled:text-stone-800 disabled:shadow-inner disabled:drop-shadow-md"
+        className="disabled:drop-shadow-mdflex flex gap-4 rounded-lg bg-green-300 bg-gradient-rd from-green-400 via-green-400 to-green-500 px-4 py-6 shadow-md drop-shadow-lg disabled:bg-stone-200 disabled:from-stone-300 disabled:via-stone-200 disabled:to-stone-200 disabled:text-stone-800 disabled:opacity-70 disabled:shadow-inner disabled:drop-shadow-md"
       >
         <ArrowUpTray />
         <span>Imagine from gallery...</span>
       </Link>
       <button
         disabled
-        className="opacity-70 bg-gradient-rd from-stone-300 via-stone-200 to-stone-200 flex gap-4 rounded-lg bg-stone-200 px-4 py-6 shadow-md drop-shadow-lg disabled:bg-stone-400 disabled:text-stone-800 disabled:shadow-inner disabled:drop-shadow-md"
+        className="disabled:drop-shadow-mdflex flex gap-4 rounded-lg bg-green-300 bg-gradient-rd from-green-400 via-green-400 to-green-500 px-4 py-6 shadow-md drop-shadow-lg disabled:bg-stone-200 disabled:from-stone-300 disabled:via-stone-200 disabled:to-stone-200 disabled:text-stone-800 disabled:opacity-70 disabled:shadow-inner disabled:drop-shadow-md"
       >
         <Camera />
         <span>Imagine something new...</span>
