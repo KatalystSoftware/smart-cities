2136e5b5ac593ef670b6847dd9a67850e02aa1a4
Mikael Siidorow <mikael.siidorow@aalto.fi>
2023-04-01 12:47:06 +0300

    Add app navigation

---
+++ src/pages/settings/index.tsx
@@ -0,0 +1,22 @@
+import { type NextPage } from "next";
+import { signOut, signIn, useSession } from "next-auth/react";
+
+const Settings: NextPage = () => {
+  const { data: sessionData } = useSession();
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-4">
+      <p className="text-center text-2xl text-black">
+        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+      </p>
+      <button
+        className="rounded-full bg-black/10 px-10 py-3 font-semibold text-black no-underline transition hover:bg-black/20"
+        onClick={sessionData ? () => void signOut() : () => void signIn()}
+      >
+        {sessionData ? "Sign out" : "Sign in"}
+      </button>
+    </div>
+  );
+};
+
+export default Settings;
