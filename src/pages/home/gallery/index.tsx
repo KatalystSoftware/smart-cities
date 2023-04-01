98be4f44836d4f2454b5b4fe2dd1e3c43f0c7381
Mikael Siidorow <mikael.siidorow@aalto.fi>
2023-04-01 22:08:47 +0300

    Dynamically map gallery from mockData

--- src/pages/home/gallery/index.tsx
+++ src/pages/home/gallery/index.tsx
@@ -1,50 +1,22 @@
 import { type NextPage } from "next";
 import Image from "next/image";
 import Link from "next/link";
-import roundabout from "public/imaginations/roundabout-before.jpg";
-import roundaboutAlt from "public/imaginations/roundabout-alt-before.jpg";
-import park from "public/imaginations/park-before.jpg";
-import wall from "public/imaginations/wall-before.jpg";
-import gasStation from "public/imaginations/gas-station-before.jpg";
-import parkingLot from "public/imaginations/parking-lot-before.jpg";
+import { mockPlaces } from "~/data";
 
 const Gallery: NextPage = () => {
   return (
     <div className="flex-1 basis-0 columns-2 gap-0 overflow-y-scroll">
-      <Link href="gallery/roundabout">
-        <Image
-          className="aspect-[9/16] object-cover"
-          src={roundabout}
-          alt="Roundabout"
-        />
-      </Link>
-      <Link href="gallery/roundabout-alt">
-        <Image
-          className="aspect-[9/16] object-cover"
-          src={roundaboutAlt}
-          alt="Roundabout"
-        />
-      </Link>
-      <Link href="gallery/park">
-        <Image className="aspect-[9/16] object-cover" src={park} alt="Park" />
-      </Link>
-      <Link href="gallery/wall">
-        <Image className="aspect-[9/16] object-cover" src={wall} alt="Wall" />
-      </Link>
-      <Link href="gallery/gas-station">
-        <Image
-          className="aspect-[9/16] object-cover"
-          src={gasStation}
-          alt="Wall"
-        />
-      </Link>
-      <Link href="gallery/parking-lot">
-        <Image
-          className="aspect-[9/16] object-cover"
-          src={parkingLot}
-          alt="Wall"
-        />
-      </Link>
+      {Object.keys(mockPlaces).map((name) => (
+        <Link key={name} href={`gallery/${name}`}>
+          <Image
+            className="aspect-[9/16] object-cover"
+            width={216}
+            height={384}
+            src={`/imaginations/${name}-original.jpg`}
+            alt={name}
+          />
+        </Link>
+      ))}
     </div>
   );
 };
